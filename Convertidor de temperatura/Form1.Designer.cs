f601a1db4796eaa6e14d7e35a9973d431d5ceb08
MeredithDaniela <mcerda0@ucol.mx>
2026-09-21 12:33:05 -0600

    Terminado

--- Convertidor de temperatura/Form1.Designer.cs
+++ Convertidor de temperatura/Form1.Designer.cs
@@ -28,12 +28,85 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            label2 = new Label();
+            txtCen = new TextBox();
+            txtfah = new TextBox();
+            btnAcep = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(12, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(176, 22);
+            label1.TabIndex = 0;
+            label1.Text = "Grados Centigrados";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.Location = new Point(14, 171);
+            label2.Name = "label2";
+            label2.Size = new Size(174, 22);
+            label2.TabIndex = 1;
+            label2.Text = "Grados Faherenheit";
+            // 
+            // txtCen
+            // 
+            txtCen.Location = new Point(194, 53);
+            txtCen.Name = "txtCen";
+            txtCen.Size = new Size(170, 23);
+            txtCen.TabIndex = 2;
+            txtCen.TextChanged += txtCen_TextChanged;
+            txtCen.KeyPress += txtCen_KeyPress;
+            // 
+            // txtfah
+            // 
+            txtfah.Location = new Point(194, 170);
+            txtfah.Name = "txtfah";
+            txtfah.Size = new Size(170, 23);
+            txtfah.TabIndex = 3;
+            txtfah.KeyPress += txtfah_KeyPress;
+            // 
+            // btnAcep
+            // 
+            btnAcep.Location = new Point(219, 265);
+            btnAcep.Name = "btnAcep";
+            btnAcep.Size = new Size(118, 45);
+            btnAcep.TabIndex = 4;
+            btnAcep.Text = "Aceptar";
+            btnAcep.UseVisualStyleBackColor = true;
+            btnAcep.Click += btnAcep_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(384, 345);
+            Controls.Add(btnAcep);
+            Controls.Add(txtfah);
+            Controls.Add(txtCen);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Convertidor de temperatura";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox txtCen;
+        private TextBox txtfah;
+        private Button btnAcep;
     }
 }
--- Convertidor de temperatura/Form1.cs
+++ Convertidor de temperatura/Form1.cs
@@ -2,9 +2,71 @@ namespace Convertidor_de_temperatura
 {
     public partial class Form1 : Form
     {
+        //Agregamos un objeto text box
+        private TextBox objTextBox = null;
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnAcep_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+        private void Conversion()
+        {
+            try
+            {
+                double grados;
+                //si se describe en la caja de textos grados centigrados...
+                if (objTextBox == txtCen)
+                {
+                    grados = Convert.ToDouble(txtCen.Text);
+                    txtfah.Text = ((grados * 9 / 5) + 32).ToString();
+                }
+                if (objTextBox == txtfah)
+                {
+                    grados = Convert.ToDouble(txtfah.Text);
+                    txtfah.Text = ((grados * 5 / 9) - 32.0).ToString();
+                }
+            }
+            catch (FormatException)
+            {
+                txtCen.Text = "0.00";
+                txtfah.Text = "32.00";
+            }
+        }
+
+        private void txtCen_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtCen_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            objTextBox = (TextBox)sender;
+            if (e.KeyChar == Convert.ToChar(13))
+            {
+                //enter
+                e.Handled = true;
+                Conversion();
+            }
+        }
+
+        private void txtfah_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            objTextBox = (TextBox)sender;
+            if (e.KeyChar == Convert.ToChar(13))
+            {
+                //enter
+                e.Handled = true;
+                Conversion();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
